c96e4685d8bf19e8855911c42a90adb42dd66693
SarveshBajaj <sbsarveshbajaj1998@gmail.com>
2017-08-20 12:34:47 +0530

    Many new changes until P5

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
 console.log('Loaded!');
+//changing text to New Value P5
+var element = document.getElementById('main-text');
+element.innerHTML='New Value';
+//Move image
+var img = document.getElementById('madi');
+var marginLeft = 0;
+//var marginRight = 0;
+function moveRight(){
+   marginLeft = marginLeft + 1;
+   img.style.marginLeft = marginLeft + 'px';
+}
+//function moveLeft(){
+  // marginRight = marginRight + 50;
+   //img.style.marginRight = marginRight + 'px';
+//}
+img.onclick = function(){
+  var interval = setInterval(moveRight, 50);//means moveRight function will be called after every 100 ms
+  //img.style.marginLeft = '100px';
+}
+//img.onclick = function(){
+  //var interval = setInterval(moveLeft, 25);//means moveRight function will be called after every 100 ms
+  //img.style.marginLeft = '100px';
+//}
